814e52a4080da91793b637127fdf3c99d191430a
xxbridgeyxx <64440508+xxbridgeyxx@users.noreply.github.com>
2020-10-28 16:09:15 +1300

    something new

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+/**
+ * name: getAllStudents
+ * @param: null
+ * @returns students []
+ */
+
 function getAllStudents(){
     students = [];
     $.ajax({
@@ -16,6 +22,11 @@ function getAllStudents(){
     return students;
 }
 
+/**
+ * @name getStudent
+ * @param student_id
+ * @returns student object
+ */
 function getStudent(id){
     student = null;
     $.ajax({
@@ -32,22 +43,57 @@ function getStudent(id){
 
     });
     return student;
+
+
 }
 
-function getEnrolement(EnrolementID){
-    enrolement = null;
+/**
+ * @name getEnrolmentInfo
+ * @param StudentID
+ * @returns enrolled boolean
+ */
+function hasEnrolled(studentID){
+    enrolled = false;
+    $.ajax({
+        type: 'Get',
+        dataType: 'JSON',
+        async: false,
+        url: 'https://balham-college-db.herokuapp.com/api/Enrolment/',
+        success: function (data) {
+            // location.reload();
+            j = 0;
+            while (j < data.length){
+                console.log(studentID);
+                enrolment = data[j];
+                if (enrolment.Student === studentID ){
+                    enrolled = true;
+                    return enrolled;
+                }
+                j = j + 1;
+            }
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+    return enrolled;
+}
+
+function getEnrolment(EnrolmentID){
+    enrolments = 0;
+    console.log(EnrolmentID);
     $.ajax({
         type: 'Get',
         dataType: 'JSON',
         async:false,
-        url: 'https://balham-college-db.herokuapp.com/api/Enrolement/'+EnrolementID,
+        url: 'https://balham-college-db.herokuapp.com/api/Enrolment/'+EnrolmentID,
         success: function (data) {
-            enrolement = data;
+            enrolments = data;
         },
         error: function (err) {
             console.log(err);
         }
 
     });
-    return enrolement;
+    return enrolments;
 }
